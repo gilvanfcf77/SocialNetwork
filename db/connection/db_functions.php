2195bed680d656715eac2ee1038882cc87960a1a
Lucas <lucas64_64@hotmail.com>
2016-11-23 21:37:24 -0200

    insert, delete, update adicionados ao dbfunctions

--- db/connection/db_functions.php
+++ db/connection/db_functions.php
@@ -10,11 +10,55 @@
 			$where = func_get_arg($i);
 			$query = "$query ".$where;
 		}
-		echo func_get_args();
+		echo $query;
 		return mysqli_query($link ,$query);
 		
 	}
 
-	select($link,"active", "1","OR","4");
+	function insert($link, $table, $value)
+	{
+		$numargs = func_num_args();
+		$query = "INSERT INTO ".$table." VALUES ( ".$value;
+		echo "Number of arguments: $numargs<br />\n";
+		for ($i = 2; $i<$numargs;$i++) {
+			$where = func_get_arg($i);
+			$query = "$query ".",".$where;
+		}
+		$query = $query.")";
+		echo $query;
+		return mysqli_query($link ,$query);
+		
+	}
+
+	function delete($link, $table, $wheres)
+	{
+		$numargs = func_num_args();
+		$query = "DELETE * FROM ".$table." WHERE ";
+		echo "Number of arguments: $numargs<br />\n";
+		for ($i = 2; $i<$numargs;$i++) {
+			$where = func_get_arg($i);
+			$query = "$query ".$where;
+		}
+		echo $query;
+		return mysqli_query($link ,$query);
+		
+	}
+
+	function update($link, $table, $value)
+	{
+		$numargs = func_num_args();
+		$query = "UPDATE ".$table." SET ( ".$value;
+		echo "Number of arguments: $numargs<br />\n";
+		for ($i = 2; $i<$numargs;$i++) {
+			$where = func_get_arg($i);
+			$query = "$query ".",".$where;
+		}
+		$query = $query.")";
+		echo $query;
+		return mysqli_query($link ,$query);
+		
+	}
+
+	// delete($link,"active", "1","OR","4");
 
  ?>
