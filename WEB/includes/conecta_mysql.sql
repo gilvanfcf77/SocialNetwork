476369c45d5283c97037bfd49908277add658dc0
gilvanfcf77 <gilvanfcf77@gmail.com>
2016-12-15 17:27:25 -0300

    considerar nosso esforço

--- WEB/includes/conecta_mysql.sql
+++
@@ -1,8 +0,0 @@
-<?php 
-	//configurações do banco de dados
-	$servidor = "localhost";
-	$usuario_bd = "root";
-	$senha_bd = "";
-	$banco = "bancofinancas";
-	$con = mysqli_connect($servidor, $usuario_bd,$senha_bd,$banco) or die ("Problema ao Conectar ao Banco de Dados");
- ?>
